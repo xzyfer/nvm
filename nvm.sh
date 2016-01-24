77e882e3bd5ac5944b67b5ea98d588541f959b0d
Michael Mifsud <xzyfer@gmail.com>
2016-01-25 00:08:02 +1100

    Update nvm.sh

--- nvm.sh
+++ nvm.sh
@@ -39,7 +39,7 @@ nvm_get_latest() {
 
 nvm_download() {
   if nvm_has "curl"; then
-    curl -q $*
+    curl -k -q $*
   elif nvm_has "wget"; then
     # Emulate curl with wget
     ARGS=$(echo "$*" | command sed -e 's/--progress-bar /--progress=bar /' \
